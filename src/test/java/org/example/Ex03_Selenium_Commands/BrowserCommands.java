3eef0881d5654c6425bbeeda44a99655e5e92622
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-12-25 17:03:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Readme.md

---
+++ src/test/java/org/example/Ex03_Selenium_Commands/BrowserCommands.java
@@ -0,0 +1,28 @@
+package org.example.Ex03_Selenium_Commands;
+
+public class BrowserCommands {
+    public static void main(String[] args) {
+
+        /*
+         *** Browser Commands ***
+         1. Opens the specified URL in the browser.
+        driver.get("https://www.google.com");
+
+         2. Retrieves the title of the current web page.
+        String title = driver.getTitle(); // Example: "Google"
+
+         3. Returns the URL of the current web page.
+        String currentUrl = driver.getCurrentUrl(); // Example: "https://www.google.com"
+
+         4. Retrieves the HTML source of the current page.
+        String pageSource = driver.getPageSource(); // Useful for debugging.
+
+         5. Closes the current browser tab.
+        driver.close(); // Use when you want to close just one tab.
+
+         6. Closes all browser windows and ends the WebDriver session.
+        driver.quit(); // Recommended for complete session cleanup.
+        */
+
+    }
+}
