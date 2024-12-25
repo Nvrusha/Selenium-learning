6c4c5edbe9b494104b423cb0a07ad1d75b5f1767
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-12-25 17:14:29 +0530

    New code

---
+++ src/test/java/org/example/Ex03_Selenium_Commands/ValidationCommands.java
@@ -0,0 +1,20 @@
+package org.example.Ex03_Selenium_Commands;
+
+public class ValidationCommands {
+    public static void main(String[] args) {
+
+        // *** Validation Commands ***
+
+        /*
+        1. Checks if the element is visible on the page.
+        boolean visible = element.isDisplayed(); // Returns true if visible.
+
+        2. Checks if the element is enabled (interactable).
+        boolean enabled = element.isEnabled(); // Useful for buttons or form inputs.
+
+        3. Checks if a checkbox or radio button is selected.
+        boolean selected = element.isSelected(); // Use for form validation.
+
+         */
+    }
+}
