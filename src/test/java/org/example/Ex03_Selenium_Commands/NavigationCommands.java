a067efbfc75d7f03de5ae5a266bdf312f55ec220
Git tutorial <nivangunevrushali.vn@gmail.com>
2024-12-25 17:05:57 +0530

    New code

---
+++ src/test/java/org/example/Ex03_Selenium_Commands/NavigationCommands.java
@@ -0,0 +1,17 @@
+package org.example.Ex03_Selenium_Commands;
+
+public class NavigationCommands {
+    /*
+     1. Navigates to a specific URL, similar to driver.get().
+     driver.navigate().to("https://www.example.com");
+
+     2. Simulates the browser's back button.
+     driver.navigate().back();
+
+     3. Simulates the browser's forward button.
+     driver.navigate().forward();
+
+     4. Refreshes the current page.
+     driver.navigate().refresh(); // Useful when the page content changes dynamically.
+    */
+}
