f65735735e5fb4c6dc7d48a813192585b2d2c87c
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-05-27 09:04:47 +0530

    27th May 2025

--- src/test/java/org/example/Projects/MakeMyTrip/Verify_Flight_Search_Functionality.java
+++ src/test/java/org/example/Projects/MakeMyTrip/Verify_Flight_Search_Functionality.java
@@ -1,12 +1,10 @@
 package org.example.Projects.MakeMyTrip;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
