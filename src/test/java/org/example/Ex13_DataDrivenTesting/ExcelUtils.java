af1129faee8ae0fb7b4c5fa8a83581178bfaec9e
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-04 08:46:11 +0530

    4th June 2025

---
+++ src/test/java/org/example/Ex13_DataDrivenTesting/ExcelUtils.java
@@ -0,0 +1,24 @@
+package org.example.Ex13_DataDrivenTesting;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.xmlbeans.impl.xb.xsdschema.Public;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class ExcelUtils {
+
+    public static FileInputStream fi;
+    public static FileOutputStream fo;
+    public static XSSFWorkbook wb;
+    public static XSSFSheet ws;
+    public static XSSFRow row;
+    public static XSSFCell cell;
+    public static CellStyle style;
+
+
+}
