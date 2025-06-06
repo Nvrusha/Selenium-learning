d8200568b131a3b6d38d55665a997cc106c07e29
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-06 08:32:35 +0530

    6th June 2025

--- src/test/java/org/example/Ex13_DataDrivenTesting/ExcelUtils.java
+++ src/test/java/org/example/Ex13_DataDrivenTesting/ExcelUtils.java
@@ -1,6 +1,8 @@
 package org.example.Ex13_DataDrivenTesting;
 
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -81,5 +83,28 @@ public class ExcelUtils {
         wb.close();
         fi.close();
     }
+
+    public static void fillGreenColor (String filePath, String sheetName, int rowNum, int colNum, String data) throws IOException {
+
+        fi = new FileInputStream(filePath);
+        wb = new XSSFWorkbook(fi);
+        ws = wb.getSheet(sheetName);
+        row = ws.getRow(rowNum);
+        cell = row.getCell(colNum);
+
+        style = wb.createCellStyle();
+
+        style.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        cell.setCellStyle(style);
+        fo = new FileOutputStream(filePath);
+        wb.write(fo);
+        fo.close();
+        wb.close();
+        fi.close();
+    }
+
+
 
 }
