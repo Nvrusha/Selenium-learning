e404ed2faad8f3ce902f6913c44fb7a84784b0cf
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-09 10:25:47 +0530

    9th June 2025

--- src/test/java/org/example/Ex13_DataDrivenTesting/ReadingPropertiesFile/ReadingKeyAndValue.java
+++ src/test/java/org/example/Ex13_DataDrivenTesting/ReadingPropertiesFile/ReadingKeyAndValue.java
@@ -3,13 +3,14 @@ package org.example.Ex13_DataDrivenTesting.ReadingPropertiesFile;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.Set;
 
-public class Practice {
+public class ReadingKeyAndValue {
     public static void main(String[] args) throws IOException {
 
         Properties properties = new Properties();
 
-        FileInputStream file = new FileInputStream(System.getProperty("user + dir") + "\\TestData\\Config.properties");
+        FileInputStream file = new FileInputStream(System.getProperty("user.dir") + "\\TestData\\Config.properties");
 
         properties.load(file);
 
@@ -19,6 +20,11 @@ public class Practice {
         String password = properties.getProperty("password");
         String timeout = properties.getProperty("timeout");
 
-        System.out.println(url + "" + browser + "" + username + "" + password + "" + timeout);
+        System.out.println(url + " " + browser + " " + username + " " + password + " " + timeout);
+
+        // Reading keys
+        Set<String> keys = properties.stringPropertyNames();
+
+        System.out.println(keys);
     }
 }
