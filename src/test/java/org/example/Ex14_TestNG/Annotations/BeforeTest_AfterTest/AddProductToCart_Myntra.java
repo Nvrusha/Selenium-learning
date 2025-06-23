a62b09966c4f46f5a500e5d8c938704d87850af3
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-23 09:05:42 +0530

    23rd June 2025

--- src/test/java/org/example/Ex14_TestNG/Annotations/BeforeTest_AfterTest/AddProductToCart_Myntra.java
+++ src/test/java/org/example/Ex14_TestNG/Annotations/BeforeTest_AfterTest/AddProductToCart_Myntra.java
@@ -3,6 +3,7 @@ package org.example.Ex14_TestNG.Annotations.BeforeTest_AfterTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -13,7 +14,7 @@ public class AddProductToCart_Myntra {
     WebDriver driver;
 
     @BeforeTest
-    void launchBrowser(){
+    void launchBrowser() {
         System.out.println("Launching browser and opening Myntra...");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -22,11 +23,17 @@ public class AddProductToCart_Myntra {
     }
 
     @Test
-    void VerifyAddingToCart (){
+    void VerifyAddingToCart() {
         driver.findElement(By.xpath("//input[@class = 'desktop-searchBar']")).sendKeys("Shoes for women");
         driver.findElement(By.xpath("//a[@class='desktop-submit']")).click();
-        driver.findElement(By.xpath("//img[contains(@title,'Back To School Shoes')]]")).click();
+        driver.findElement(By.xpath("//img[contains(@title,'Back To School Shoes')]")).click();
         driver.findElement(By.xpath("//p[text() ='7']")).click();
         driver.findElement(By.xpath("//div[contains(@class,'flex pdp-center')]")).click();
+    }
+
+    @AfterTest
+    void tearDown() {
+        System.out.println("Closing browser...");
+        driver.quit();
     }
 }
