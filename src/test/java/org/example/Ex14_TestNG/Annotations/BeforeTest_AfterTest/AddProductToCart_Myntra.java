54ad3b0c247cb35d1fb7760f58ae14f876fa0c4b
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-23 09:12:55 +0530

    23rd June 2025

--- src/test/java/org/example/Ex14_TestNG/Annotations/BeforeTest_AfterTest/AddProductToCart_Myntra.java
+++ src/test/java/org/example/Ex14_TestNG/Annotations/BeforeTest_AfterTest/AddProductToCart_Myntra.java
@@ -2,7 +2,10 @@ package org.example.Ex14_TestNG.Annotations.BeforeTest_AfterTest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -23,11 +26,26 @@ public class AddProductToCart_Myntra {
     }
 
     @Test
-    void VerifyAddingToCart() {
-        driver.findElement(By.xpath("//input[@class = 'desktop-searchBar']")).sendKeys("Shoes for women");
+    void VerifyAddingToCart() throws InterruptedException {
+        System.out.println("Searching for shoes...");
+        driver.findElement(By.xpath("//input[@class='desktop-searchBar']")).sendKeys("Shoes for women");
         driver.findElement(By.xpath("//a[@class='desktop-submit']")).click();
+
+        Thread.sleep(3000);  // Wait for products to load
+
+        System.out.println("Selecting a product...");
         driver.findElement(By.xpath("//img[contains(@title,'Back To School Shoes')]")).click();
-        driver.findElement(By.xpath("//p[text() ='7']")).click();
+
+        Thread.sleep(3000);  // Wait for product page to load
+
+        System.out.println("Selecting shoe size...");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        WebElement sizeButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//button[contains(@class,'size-buttons-size-button') and text()='7']")));
+        sizeButton.click();
+
+        System.out.println("Adding to cart...");
         driver.findElement(By.xpath("//div[contains(@class,'flex pdp-center')]")).click();
     }
 
