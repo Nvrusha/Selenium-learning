300ef066b5a97d8114a1838c08b15d17411d2c81
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-06-24 08:49:24 +0530

    24th June 2025

---
+++ src/test/java/org/example/Ex14_TestNG/Annotations/BeforeTest_AfterTest/DriverManager.java
@@ -0,0 +1,7 @@
+package org.example.Ex14_TestNG.Annotations.BeforeTest_AfterTest;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+    public static WebDriver driver;  // shared across tests
+}
