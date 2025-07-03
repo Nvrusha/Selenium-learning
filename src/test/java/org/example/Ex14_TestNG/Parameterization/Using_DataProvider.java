a751a15b23ea0edebfcc250378a0c27fd7432c69
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-07-03 22:14:33 +0530

    3rd July 2025

--- src/test/java/org/example/Ex14_TestNG/Parameterization/Using_DataProvider.java
+++ src/test/java/org/example/Ex14_TestNG/Parameterization/Using_DataProvider.java
@@ -1,5 +1,37 @@
 package org.example.Ex14_TestNG.Parameterization;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class Using_DataProvider {
 
+    WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        System.out.println();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    void login(){
+        driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("abc@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("User1234");
+
+    }
+
+    @AfterClass
+    void tearDown(){
+        driver.quit();
+    }
 }
