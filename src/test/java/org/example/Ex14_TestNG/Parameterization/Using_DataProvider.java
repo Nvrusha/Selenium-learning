a83b3a0e06b5cba5b3fc2ecfe4b48ba2ee829a62
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-07-07 20:30:31 +0530

    7th July 2025

--- src/test/java/org/example/Ex14_TestNG/Parameterization/Using_DataProvider.java
+++ src/test/java/org/example/Ex14_TestNG/Parameterization/Using_DataProvider.java
@@ -3,6 +3,7 @@ package org.example.Ex14_TestNG.Parameterization;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
@@ -25,11 +26,21 @@ public class Using_DataProvider {
     void login() throws InterruptedException {
         driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
         driver.manage().window().maximize();
-        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("abc@gmail.com");
-        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("User1234");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("nivangunevrushali.vn@gmail.com");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Vrusha123$");
         driver.findElement(By.xpath("//input[@value='Login']")).click();
 
         Thread.sleep(2000);
+
+        boolean myAccountPage = driver.findElement(By.xpath("//h2[normalize-space(text())='My Account']")).isDisplayed();
+
+        if (myAccountPage){
+            driver.findElement(By.xpath("//div[@class='list-group']//a[contains(text(), 'Logout')]")).click();
+            Assert.assertTrue(true);
+        }
+        else {
+            Assert.fail();
+        }
 
 
 
