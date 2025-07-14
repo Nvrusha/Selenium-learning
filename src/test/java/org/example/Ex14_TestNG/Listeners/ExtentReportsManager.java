459f8b7c0108b66b581f9559c1fcb37ad3865874
Git tutorial <nivangunevrushali.vn@gmail.com>
2025-07-14 22:20:30 +0530

    14th July 2025

--- src/test/java/org/example/Ex14_TestNG/Listeners/ExtentReportsManager.java
+++ src/test/java/org/example/Ex14_TestNG/Listeners/ExtentReportsManager.java
@@ -2,10 +2,12 @@ package org.example.Ex14_TestNG.Listeners;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class ExtentReportsManager implements ITestListener {
 
@@ -29,6 +31,18 @@ public class ExtentReportsManager implements ITestListener {
         extent.setSystemInfo("Environment", "QA");
         extent.setSystemInfo("Browser", "Chrome 123.0");
         extent.setSystemInfo("OS", "Windows 11");
+    }
+
+    // ✅ Called when a test method passes
+    public void onTestSuccess(ITestResult result) {
+        test = extent.createTest(result.getName());
+        test.log(Status.PASS, "Test case PASSED is:" + result.getName());
+    }
+
+    // ❌ Called when a test method fails
+    public void onTestFailure(ITestResult result) {
+        test = extent.createTest(result.getName());
+        test.log(Status.FAIL, "Test case FAILED cause is:" + result.getThrowable());
     }
 
 }
